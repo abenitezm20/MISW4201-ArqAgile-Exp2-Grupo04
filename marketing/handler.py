42c3d5ea4db107a401388eceec546ee28e4e5589
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2023-03-18 15:42:05 -0500

    Ajuste lectura canal autorizado

--- marketing/handler.py
+++ marketing/handler.py
@@ -2,23 +2,40 @@ import time
 import sys
 import json
 import redis
+import requests
 
 from faker import Faker
 
 sys.path.insert(0, '../')
 
-CANAL_MARKETING = 'marketing_receptor';
-CANAL_OFERTAS = 'marketing_ofertas';
+## Configuración Login
+URL_LOGIN ='http://127.0.0.1:5000/login'
+URL_OBTENER_CANAL ='http://127.0.0.1:5000/api/canales'
+USUARIO_AUTORIZADOR = { "usuario": "ccp", "contrasena": "usuario1"}
+
+## Configuración queue marketing
+CANAL_OFERTAS = 'marketing_ofertas'
 REDIS_CONNECTION = redis.Redis(host='localhost', port=6379, db=0)
 
 redis_db = REDIS_CONNECTION
 sub = redis_db.pubsub()
-sub.subscribe(CANAL_MARKETING)
 
 class Handler():
     @staticmethod
     def escuchar():
-        print('Escuchando mensajes del canal', CANAL_MARKETING)
+        
+        # Obtiene token autorizador
+        login = requests.post(URL_LOGIN, json = USUARIO_AUTORIZADOR)
+        respLogin = json.loads(login.content.decode('utf-8'))
+        token = respLogin['data']
+
+        # Obtiene canal marketing para escuchar peticiones
+        respCanal = requests.get(URL_OBTENER_CANAL, headers = {"Authorization": 'Bearer ' + token})
+        canales_marketing = json.loads(respCanal.content.decode('utf-8'))['data']
+        canal_autorizado = canales_marketing[0]
+
+        print('Escuchando mensajes del canal', canal_autorizado)
+        sub.subscribe(canal_autorizado)
         Handler.leer_mensajes()
 
     @staticmethod
