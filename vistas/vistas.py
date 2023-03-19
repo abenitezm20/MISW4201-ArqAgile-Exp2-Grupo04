d65ff24fa089176ffbf4ae0cba7aea7d741c20d6
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2023-03-19 11:22:28 -0500

    Se realiza la integracion del componete principal

--- vistas/vistas.py
+++ vistas/vistas.py
@@ -1,10 +1,18 @@
 import logging
 from flask import request
-from flask_jwt_extended import jwt_required, create_access_token, get_jwt
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt, get_jwt_identity
 from flask_restful import Resource
 import hashlib
 from modelos import db, Usuario, OTP
 from Logica.Logica import Logica
+import requests
+import json
+from handler import Handler
+
+
+
+login_autorizador='http://127.0.0.1:5001/login'
+canales_autorizador='http://127.0.0.1:5001/api/canales'
 
 logging.basicConfig(level=logging.INFO, filename='audit.log', encoding='utf-8', format='%(asctime)s %(message)s')
 
@@ -14,6 +22,23 @@ class VistaLogIn(Resource):
         contrasena_encriptada = hashlib.md5(request.json["contrasena"].encode('utf-8')).hexdigest()
         usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
                                        Usuario.contrasena == contrasena_encriptada).first()
+        db.session.commit()
+        if usuario is None:
+            logging.error(f'El usuario no existe')
+            return "El usuario no existe", 404
+        else:
+            otp = Logica.generarCodigoOTP()
+            nuevo_otp = OTP(otp=otp, id_usuario=usuario.id, usuario=usuario.usuario)
+            db.session.add(nuevo_otp)
+            db.session.commit()
+            logging.info('Se genero codigo OTP para usuario: '+usuario.usuario)
+            return otp
+        
+class VistaLogInUsuario(Resource):
+    
+    def post(self):
+        usuario = Usuario.query.filter(Usuario.usuario == request.json["usuario"],
+                                       Usuario.contrasena == request.json["contrasena"]).first()
         db.session.commit()
         if usuario is None:
             logging.error(f'El usuario no existe')
@@ -46,3 +71,30 @@ class VistaValidarOTP(Resource):
             db.session.delete(otp_almacenado)
             db.session.commit()
             return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": otp_almacenado.id_usuario}
+
+
+class VistaCrearOferta(Resource):
+    
+    @jwt_required()
+    def post(self):
+        
+        oferta={"data":{'product':request.json["producto"],"oferta" :request.json["oferta"]}}
+
+        #Validar usuario en el autorizador
+        id_usuario = get_jwt_identity()
+        usuario = Usuario.query.get_or_404(id_usuario)
+        json_usuario={"usuario": usuario.usuario, "contrasena": usuario.contrasena}
+        registro_autorizador = requests.post(login_autorizador, json = json_usuario )
+        respuesta_autorizador = json.loads(registro_autorizador.content.decode('utf-8'))
+        token_autorizador = respuesta_autorizador['data']
+        
+        #Solicitar canales al autorizador
+        respCanal = requests.get(canales_autorizador, headers = {"Authorization": 'Bearer ' + token_autorizador})
+        respuesta_canales = json.loads(respCanal.content.decode('utf-8'))['data']
+        for canal in respuesta_canales:
+            Handler.publicar(oferta=json.dumps(oferta).encode('utf-8'),canal=canal, id_usuario=str(id_usuario))
+
+        return {"respuesta_canales":respuesta_canales}
+        
+
+
